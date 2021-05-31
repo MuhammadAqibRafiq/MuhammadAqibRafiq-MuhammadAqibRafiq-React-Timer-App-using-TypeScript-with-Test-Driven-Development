cd579f31aae5b680937b65321af22518ee1cd537
Muhammadaqibrafiq <aqibrafiq001@outlook.com>
2021-06-01 00:02:13 +0500

    first commit

---
+++ src/component/App/App.tsx
@@ -0,0 +1,14 @@
+  
+import React from "react";
+import "./App.css";
+import Timer from "../Timer/Timer";
+
+
+const App = () => (
+  <div className='app-container' >
+    <h1 id="ctr">TIMER APP</h1>
+   <Timer/>
+  </div>
+);
+
+export default App;
